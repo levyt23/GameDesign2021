cc387b5df3d29f0d74e7bee8f44570c066bc9055
Taylor <levyt23@greenhill.org>
2021-06-08 11:10:58 -0500

    first commit

---
+++ multiplication.py
@@ -0,0 +1,14 @@
+#taylor Levy
+
+base = 2
+var2 = 7
+print (1 * base)
+print (2 * base)
+print (3 * base)
+print (4 * base)
+print (5 * base)
+print (6 * base)
+print (7 * base)
+print (8 * base)
+print (9 * base)
+print (10 * base)
